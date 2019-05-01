e4dc025b91b6fd223e392db728be7dde97b715f4
Nada Ashraf <nadaashraf11@icloud.com>
2019-05-02 01:35:27 +0200

    added huffman.hpp

---
+++ huffman.hpp
@@ -0,0 +1,44 @@
+#ifndef HUFFMAN_HPP
+#define HUFFMAN_HPP
+
+#include <vector>
+#include <utility>
+#include <string>
+#include <map>
+
+struct Symbol
+{
+    unsigned char val; //greyscale value
+    float p;           //probability
+};
+
+struct Node //tree node
+{
+    Symbol s;
+    Node *left = nullptr;
+    Node *right = nullptr;
+};
+
+class Huffman
+{
+  public:
+    float computeProb();
+    int encode();
+    void decode();
+
+  private:
+    Node *tree;
+    std::vector<unsigned char> input;
+    int width = 0, height = 0;
+    std::vector<Symbol> probability;
+    std::map<unsigned char, std::string> codeTable; //greyscale value : code
+    std::vector<unsigned int> encoded;
+    std::vector<unsigned char> decoded;
+
+    void readInput();
+    void buildTree();
+    void printEncoded();
+    void printDecoded();
+};
+
+#endif
