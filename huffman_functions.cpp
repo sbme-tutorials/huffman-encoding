e959bc916c2b55801168a5f4584bd8095772dd57
Nada Ashraf <nadaashraf11@icloud.com>
2019-05-02 01:41:25 +0200

    added huffman_functions.cpp , file for functions implementation

---
+++ huffman_functions.cpp
@@ -0,0 +1,31 @@
+#include "huffman.hpp"
+
+// functions definitions here
+
+float Huffman::computeProb() // computes entropy and probability of each greyscale value
+{
+}
+
+int Huffman::encode() // calls reaInput() , computeEntropy(), buildTree() , printEncoded()
+{
+}
+
+void Huffman::decode() // calls printDecoded()
+{
+}
+
+void Huffman::buildTree()
+{
+}
+
+void Huffman::readInput()
+{
+}
+
+void Huffman::printEncoded()
+{
+}
+
+void Huffman::printDecoded()
+{
+}
