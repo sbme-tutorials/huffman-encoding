954872dcd11c88de7054247aef6efeb4117970c0
Khaled Maher <khaled.bedda98@eng-st.cu.edu.eg>
2019-05-03 00:19:24 +0200

    Implementing buildtree func

--- huffman.hpp
+++ huffman.hpp
@@ -6,39 +6,34 @@
 #include <string>
 #include <map>
 
-struct Symbol
-{
-    unsigned char val; //greyscale value
-    float p;           //probability
-};
-
 struct Node //tree node
 {
-    Symbol s;
-    Node *left = nullptr;
-    Node *right = nullptr;
+  unsigned char val; //greyscale value
+  float p;           //probability
+  Node *left = nullptr;
+  Node *right = nullptr;
 };
 
 class Huffman
 {
-  public:
-    float computeProb();
-    int encode();
-    void decode();
+public:
+  float computeProb();
+  int encode();
+  void decode();
 
-  private:
-    Node *tree;
-    std::vector<unsigned char> input;
-    int width = 0, height = 0;
-    std::vector<Symbol> probability;
-    std::map<unsigned char, std::string> codeTable; //greyscale value : code
-    std::vector<unsigned int> encoded;
-    std::vector<unsigned char> decoded;
+private:
+  Node *tree;
+  std::vector<unsigned char> input;
+  int width = 0, height = 0;
+  std::vector<Symbol> probability;
+  std::map<unsigned char, std::string> codeTable; //greyscale value : code
+  std::vector<unsigned int> encoded;
+  std::vector<unsigned char> decoded;
 
-    void readInput();
-    void buildTree();
-    void printEncoded();
-    void printDecoded();
+  void readInput();
+  void buildTree();
+  void printEncoded();
+  void printDecoded();
 };
 
 #endif
--- huffman_functions.cpp
+++ huffman_functions.cpp
@@ -1,5 +1,5 @@
 #include "huffman.hpp"
-
+#include <bits/stdc++.h>
 // functions definitions here
 
 float Huffman::computeProb() // computes entropy and probability of each greyscale value
@@ -16,6 +16,36 @@ void Huffman::decode() // calls printDecoded()
 
 void Huffman::buildTree()
 {
+    // Create a min heap & inserts all characters of data[]
+    priority_queue<Node, vector<Node>, value_compare> min_Heap;
+
+    for (int i = 0; i < input.size; ++i) //Build Heap
+        min_Heap.push(new Node(value[i],p[i] , nullptr , nullptr);
+    // Iterate while size of heap doesn't become 1
+    while (min_Heap.size() != 1) {
+            // Extract the two minimum
+            // freq items from min heap
+            left = min_Heap.top();
+            min_Heap.pop();
+
+            right = min_Heap.top();
+            min_Heap.pop();
+
+            // Create a new internal node with frequency equal to the sum of the
+            // two nodes frequencies. Make the two extracted node as left and right children
+            // of this new node. Add this node
+            // to the min heap '$' is a special value
+            // for internal nodes, not used
+            top = new MinHeapNode('$', left->freq + right->freq);
+            top->left = left;
+            top->right = right;
+            min_Heap.push(top);
+    }
+    //  The remaining node is the 
+    // root node and the tree is complete. 
+
+return   min_Heap.top ; 
+    
 }
 
 void Huffman::readInput()
