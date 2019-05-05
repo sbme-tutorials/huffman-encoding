6bf7a6c5c4eeb6cc09100910df49b9bfe1993568
Nada Ashraf <nadaashraf11@icloud.com>
2019-05-05 16:46:37 +0200

    solved some errors

--- huffman.hpp
+++ huffman.hpp
@@ -16,7 +16,6 @@ struct Node //tree node
 
 class Huffman
 {
-<<<<<<< HEAD
   public:
     float computeProb();
     double encode();
@@ -35,29 +34,7 @@ class Huffman
     void buildTree();
     void printEncoded();
     void printDecoded();
-    void getCodeTable(Node *parent, unsigned char *arr, int index)
-=======
-public:
-  void computeProb();
-  int encode();
-  void decode();
-
-private:
-  Node *tree;
-  std::vector<unsigned char> input;
-  std::vector<unsigned char> pixelsArray ;
-  int width = 0, height = 0;
-  std::vector<int> probability;
-  std::map<unsigned char, std::string> codeTable; //greyscale value : code
-  std::vector<unsigned int> encoded;
-  std::vector<unsigned char> decoded;
-
-  float computeEntropy();
-  void readInput();
-  void buildTree();
-  void printEncoded();
-  void printDecoded();
->>>>>>> 90e85c7c98056ef1b9d8ae2473bf7970e2703a59
+    void getCodeTable(Node *parent, unsigned char *arr, int index);
 };
 
 #endif
--- huffman_functions.cpp
+++ huffman_functions.cpp
@@ -1,28 +1,19 @@
 #include "huffman.hpp"
-<<<<<<< HEAD
-#include <map>
-#include <vector>
-#include "math.h"
-#include <algorithm>
-#include <iostream>
-#include <bits/stdc++.h>
-=======
 #include <bits/stdc++.h>
 #include <stdlib.h>
 #include <iostream>
->>>>>>> 90e85c7c98056ef1b9d8ae2473bf7970e2703a59
 // functions definitions here
 
-void Huffman::computeProb()
+/*void Huffman::computeProb()
 {
     for (int i = 0; i < 256; i++)
         probability[i] = 0;
 
     for (int i = 0; i < input.size; i++)
         probability[std::atof(input[i])]++;
-}
+}*/
 
-float Huffman::computeEntropy() // computes entropy and probability of each greyscale value
+float Huffman::computeProb() // computes entropy and probability of each greyscale value
 {
     unsigned long long pixels = input.size(); //total number of pixels in the pic
     float entropy = 0;
@@ -74,8 +65,11 @@ void Huffman::buildTree()
     // Create a min heap & inserts all characters of input data[]
     priority_queue<Node, std::vector<Node>, compare> min_Heap;
 
-    for (int i = 0; i < Huffman::input.size; ++i) //Build Heap
-        min_Heap.push(new Node{pixelsArray[i],probability[ std::atof(input[i]) ] , nullptr , nullptr};
+        //build heap
+    for (auto pixel : probability)
+    {
+        minHeap.push(new Node {pixel,probability[pixel] , nullptr , nullptr});
+    }
     // Iterate while size of heap doesn't become 1
     while (minHeap.size() != 1) {
             // Extract the two minimum
@@ -120,30 +114,34 @@ void Huffman::getCodeTable(Node *parent, unsigned char *arr, int index)
 // myfile >> 
 void Huffman::readInput()
 {
-  int width = 0, height = 0;
+   //./hw_p3 -encode
   std::string intensity;
-  int maxIntensity;
-  int c;
+  unsigned char maxIntensity;
+  //int c;
   cin >> intensity >> width >> height >> maxIntensity;
+  //spaces
   for (int i = 0; i < width * height; i++)
   {
-    int temp;
-    cin >> temp;
-    msg.push_back(temp);
+    unsigned char temp;
+    cin >> temp ;
+    //cin >> temp >> " ";
+    input.push_back(temp);
   }    
 }
-//./hw_p3 -encode
 void Huffman::printEncoded()
 {
 }
 
 void Huffman::printDecoded(std::string outputfile) //input/output with files
-{
-    string directory = "./output/" + outputfile + ".pgm";
+{ 
+    std::string intensity;
+    unsigned char maxIntensity;
+    std::string directory = "./output/" + outputfile + ".pgm";
     ofstream myfile;
     myfile.open(directory);
     //print file specs width, heigth
     //print size of table code words && table of codewords
+    myfile << intensity << std::endl << width << height << std::endl << maxIntensity ;
     for(auto e: decoded) {
       myfile << (int)e << " ";
     }
