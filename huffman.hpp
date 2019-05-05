54c3c2f31a35bbee603375e40c644d783fef5bc8
Nada Ashraf <nadaashraf11@icloud.com>
2019-05-05 16:52:35 +0200

    edited huffman.hpp

--- huffman.hpp
+++ huffman.hpp
@@ -16,7 +16,6 @@ struct Node //tree node
 
 class Huffman
 {
-<<<<<<< HEAD
   public:
     float computeProb();
     double encode();
@@ -36,28 +35,6 @@ class Huffman
     void printEncoded();
     void printDecoded();
     void getCodeTable(Node *parent, unsigned char *arr, int index);
-=======
-public:
-  void computeProb();
-  int encode();
-  void decode();
-
-private:
-  Node *tree;
-  std::vector<unsigned char> input;
-  std::vector<unsigned char> pixelsArray ;
-  int width = 0, height = 0;
-  std::map<unsigned char , unsigned long long> probability;
-  std::map<unsigned char, std::string> codeTable; //greyscale value : code
-  std::vector<unsigned int> encoded;
-  std::vector<unsigned char> decoded;
-
-  float computeEntropy();
-  void readInput();
-  void buildTree();
-  void printEncoded();
-  void printDecoded();
->>>>>>> 319138d980d627f9d7602d1dcace1a9fac790a2d
 };
 
 #endif
