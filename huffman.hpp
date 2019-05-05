d3c7f1efe2eea4b9ca1aba37f3b09e74ec3d087e
Nada Ashraf <nadaashraf11@icloud.com>
2019-05-05 13:49:53 +0200

    implemented and modified some functions

--- huffman.hpp
+++ huffman.hpp
@@ -16,7 +16,6 @@ struct Node //tree node
 
 class Huffman
 {
-<<<<<<< HEAD
   public:
     float computeProb();
     double encode();
@@ -24,10 +23,10 @@ class Huffman
 
   private:
     Node *tree;
-    std::vector<unsigned char> input;
+    std::vector<unsigned char> input ;
     int width = 0, height = 0;
     std::map<unsigned char, unsigned long long > probability;
-    std::map<unsigned char, std::string> codeTable; //greyscale value : code
+    std::map<unsigned char, std::vector<unsigned char>> codeTable; //greyscale value : code
     std::vector<std::string> encoded;
     std::vector<unsigned char> decoded;
 
@@ -35,28 +34,7 @@ class Huffman
     void buildTree();
     void printEncoded();
     void printDecoded();
-    void getCodeTable();
-    void printCodeTable();
-=======
-public:
-  float computeProb();
-  int encode();
-  void decode();
-
-private:
-  Node *tree;
-  std::vector<unsigned char> input;
-  int width = 0, height = 0;
-  std::vector<Symbol> probability;
-  std::map<unsigned char, std::string> codeTable; //greyscale value : code
-  std::vector<unsigned int> encoded;
-  std::vector<unsigned char> decoded;
-
-  void readInput();
-  void buildTree();
-  void printEncoded();
-  void printDecoded();
->>>>>>> 954872dcd11c88de7054247aef6efeb4117970c0
+    void getCodeTable(Node *parent, unsigned char *arr, int index)
 };
 
 #endif
--- huffman_functions.cpp
+++ huffman_functions.cpp
@@ -1,14 +1,10 @@
 #include "huffman.hpp"
-<<<<<<< HEAD
 #include <map>
 #include <vector>
 #include "math.h"
 #include <algorithm>
 #include <iostream>
-
-=======
 #include <bits/stdc++.h>
->>>>>>> 954872dcd11c88de7054247aef6efeb4117970c0
 // functions definitions here
 
 float Huffman::computeProb() // computes entropy and probability of each greyscale value
@@ -29,7 +25,7 @@ float Huffman::computeProb() // computes entropy and probability of each greysca
     return -1 * entropy;
 }
 
-double Huffman::encode() // calls reaInput() , computeEntropy(), buildTree() , printEncoded()
+double Huffman::encode()
 {
     readInput();
     computeProb();
@@ -42,7 +38,6 @@ double Huffman::encode() // calls reaInput() , computeEntropy(), buildTree() , p
         compressionRatio += code.size();
     }
     printEncoded();
-    printCodeTable();
     return compressionRatio;
 }
 
@@ -50,57 +45,88 @@ void Huffman::decode() // calls printDecoded()
 {
 }
 
-void Huffman::buildTree()
-{
-    // Create a min heap & inserts all characters of data[]
-    priority_queue<Node, vector<Node>, value_compare> min_Heap;
 
-    for (int i = 0; i < input.size; ++i) //Build Heap
-        min_Heap.push(new Node(value[i],p[i] , nullptr , nullptr);
+void Huffman::buildTree()
+{ 
+    // Create a min heap & inserts all characters of input data[]
+    std::priority_queue<Node,std::greater> minHeap;
+    //build heap
+    for (auto pixel : probability)
+    {
+        minHeap.push(new Node {pixel,probability[pixel] , nullptr , nullptr});
+    }
     // Iterate while size of heap doesn't become 1
-    while (min_Heap.size() != 1) {
+    while (minHeap.size() != 1) {
             // Extract the two minimum
             // freq items from min heap
-            left = min_Heap.top();
-            min_Heap.pop();
-
-            right = min_Heap.top();
-            min_Heap.pop();
-
+            Node left = minHeap.top();
+            minHeap.pop();
+            Node right = minHeap.top();
+            minHeap.pop();
             // Create a new internal node with frequency equal to the sum of the
             // two nodes frequencies. Make the two extracted node as left and right children
-            // of this new node. Add this node
-            // to the min heap '$' is a special value
+            // of this new node. Add this node to the min heap '$' is a special value
             // for internal nodes, not used
-            top = new MinHeapNode('$', left->freq + right->freq);
-            top->left = left;
-            top->right = right;
-            min_Heap.push(top);
+            Node parent = new Node('$', left.p + right.p,left,right);
+            minHeap.push(parent);
     }
     //  The remaining node is the 
     // root node and the tree is complete. 
-
-return   min_Heap.top ; 
-    
+    tree = parent;
+    unsigned char arr[1000]; //arbitrary size
+    getCodeTable(tree,arr,0);
 }
 
-void Huffman::getCodeTable()
+void Huffman::getCodeTable(Node *parent, unsigned char *arr, int index)
 {
+    if (parent->left)
+    {
+        arr[index] = '0';
+        getCodeTable(parent->left,arr,index+1);
+    }
+    
+    if (parent->right)
+    {
+        arr[index] = '1';
+        getCodeTable(parent->right,arr,index+1);
+    }
+    std::vector<unsigned char> binaryCode(arr, arr + index);
+    codeTable[parent->val] = binaryCode;
 }
 
-
+// ifstream myfile;
+// myfile.open();
+// myfile >> 
 void Huffman::readInput()
 {
+  int width = 0, height = 0;
+  std::string intensity;
+  int maxIntensity;
+  int c;
+  cin >> intensity >> width >> height >> maxIntensity;
+  for (int i = 0; i < width * height; i++)
+  {
+    int temp;
+    cin >> temp;
+    msg.push_back(temp);
+  }    
 }
-
+//./hw_p3 -encode
 void Huffman::printEncoded()
 {
 }
 
-void Huffman::printDecoded() //input/output with files
+void Huffman::printDecoded(std::string outputfile) //input/output with files
 {
+    string directory = "./output/" + outputfile + ".pgm";
+    ofstream myfile;
+    myfile.open(directory);
+    //print file specs width, heigth
+    //print size of table code words && table of codewords
+    for(auto e: decoded) {
+      myfile << (int)e << " ";
+    }
+    myfile.close();
 }
 
-void Huffman::printCodeTable()
-{
-}
+
